fd6d702b17f1344828aff1602abf2a73edcf0a47
Rodrigo Duran <rads2995@gmail.com>
2025-02-10 23:32:15 -0500

    Removing TOML file and serde dependency in favor of scripting with lua.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ mod bindings {
     pub(crate) mod ipopt;
     pub(crate) mod lua;
 }
-mod math {
-    pub(crate) mod gaussian_quadrature;
-}
 
 use bindings::ipopt::helper::{eval_f, eval_g, eval_grad_f, eval_h, eval_jac_g};
 use bindings::ipopt::{
@@ -15,42 +12,20 @@ use bindings::ipopt::{
     IpoptSolve,
 };
 
-use serde::Deserialize;
-use std::ffi::CStr;
-use std::fs;
-
-#[derive(Deserialize)]
-struct Config {
-    ipopt: Ipopt,
-}
-
-#[derive(Deserialize)]
-struct Ipopt {
-    tol: ipnumber,
-    mu_strategy: std::ffi::CString,
-    output_file: std::ffi::CString,
-    linear_solver: std::ffi::CString,
-    index_style: u32,
-    x: Vec<ipnumber>,
-    x_l: Vec<ipnumber>,
-    x_u: Vec<ipnumber>,
-    g_l: Vec<ipnumber>,
-    g_u: Vec<ipnumber>,
-}
-
 fn main() {
-    let config_file: String =
-        fs::read_to_string("Settings.toml").expect("Unable to read configuration file");
-
-    let mut config_data: Config = toml::from_str(&config_file).unwrap();
+    let mut x_l: Vec<f64> = std::vec!(1.0, 1.0, 1.0, 1.0);
+    let mut x_u: Vec<f64> = std::vec!(5.0, 5.0, 5.0, 5.0);
+    let mut g_l: Vec<f64> = std::vec!(25.0, 40.0);
+    let mut g_u: Vec<f64> = std::vec!(2e19, 40.0);
+    let mut x: Vec<f64> = std::vec!(1.0, 5.0, 5.0, 1.0);
 
     // Set the number of decision variables
-    assert!(config_data.ipopt.x_l.len() == config_data.ipopt.x_u.len());
-    let n: usize = config_data.ipopt.x_l.len();
+    assert!(x_l.len() == x_u.len());
+    let n: usize = x_l.len();
 
     // Set the number of constraints
-    assert!(config_data.ipopt.g_l.len() == config_data.ipopt.g_u.len());
-    let m: usize = config_data.ipopt.g_l.len();
+    assert!(g_l.len() == g_u.len());
+    let m: usize = g_l.len();
 
     // Set the number of nonzeroes in the Jacobian and Hessisan
     let num_elem_jac: i32 = 8;
@@ -60,12 +35,12 @@ fn main() {
     let mut obj: [ipnumber; 1] = [0.0];
 
     // Create heap-allocated arrays to store IPOPT obtained values
-    let mut mult_g: Vec<ipnumber> = vec![0.0; config_data.ipopt.g_l.len()];
-    let mut mult_x_l: Vec<ipnumber> = vec![0.0; config_data.ipopt.x_l.len()];
-    let mut mult_x_u: Vec<ipnumber> = vec![0.0; config_data.ipopt.x_u.len()];
+    let mut mult_g: Vec<ipnumber> = vec![0.0; g_l.len()];
+    let mut mult_x_l: Vec<ipnumber> = vec![0.0; x_l.len()];
+    let mut mult_x_u: Vec<ipnumber> = vec![0.0; x_u.len()];
 
     // Verify that all stack and heap-allocated arrays have the correct dimensions
-    assert!(config_data.ipopt.x.len() == n);
+    assert!(x.len() == n);
     assert!(obj.len() == 1);
     assert!(mult_g.len() == m);
     assert!(mult_x_l.len() == n);
@@ -77,14 +52,14 @@ fn main() {
     unsafe {
         let nlp: IpoptProblem = CreateIpoptProblem(
             n as ipindex,
-            config_data.ipopt.x_l.as_mut_ptr(),
-            config_data.ipopt.x_u.as_mut_ptr(),
+            x_l.as_mut_ptr(),
+            x_u.as_mut_ptr(),
             m as ipindex,
-            config_data.ipopt.g_l.as_mut_ptr(),
-            config_data.ipopt.g_u.as_mut_ptr(),
+            g_l.as_mut_ptr(),
+            g_u.as_mut_ptr(),
             num_elem_jac as ipindex,
             num_elem_hess as ipindex,
-            config_data.ipopt.index_style as ipindex,
+            0 as ipindex,
             Some(eval_f),
             Some(eval_g),
             Some(eval_grad_f),
@@ -92,26 +67,13 @@ fn main() {
             Some(eval_h),
         );
 
-        AddIpoptNumOption(nlp, c"tol".as_ptr(), config_data.ipopt.tol);
-        AddIpoptStrOption(
-            nlp,
-            c"mu_strategy".as_ptr(),
-            config_data.ipopt.mu_strategy.as_ptr(),
-        );
-        AddIpoptStrOption(
-            nlp,
-            c"output_file".as_ptr(),
-            config_data.ipopt.output_file.as_ptr(),
-        );
-        AddIpoptStrOption(
-            nlp,
-            c"linear_solver".as_ptr(),
-            config_data.ipopt.linear_solver.as_ptr(),
-        );
+        AddIpoptNumOption(nlp, c"tol".as_ptr(), 3.82e-6);
+        AddIpoptStrOption(nlp, c"mu_strategy".as_ptr(), c"adaptive".as_ptr());
+        AddIpoptStrOption(nlp, c"linear_solver".as_ptr(), c"mumps".as_ptr());
 
         let status: ApplicationReturnStatus = IpoptSolve(
             nlp,
-            config_data.ipopt.x.as_mut_ptr(),
+            x.as_mut_ptr(),
             core::ptr::null_mut(),
             obj.as_mut_ptr(),
             mult_g.as_mut_ptr(),
@@ -152,11 +114,11 @@ fn main() {
 
         let mut len: usize = 0;
 
-        let str_ptr = bindings::lua::luaL_tolstring(lua_state, -1, &mut len);
+        let str_ptr: *const i8 = bindings::lua::luaL_tolstring(lua_state, -1, &mut len);
 
-        let c_str = CStr::from_ptr(str_ptr);
+        let c_str: &std::ffi::CStr = std::ffi::CStr::from_ptr(str_ptr);
 
-        let rust_str = c_str.to_string_lossy().into_owned();
+        let rust_str: String = c_str.to_string_lossy().into_owned();
 
         dbg!(rust_str);
 
